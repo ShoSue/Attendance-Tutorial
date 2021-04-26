c22823dcb9720240ccec53a0caf6247fda6d01fb
ShoSue <shosue15m04060@gmail.com>
2021-04-26 08:29:46 +0000

    10.7finished

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,14 @@ module ApplicationHelper
       page_name + " | " + base_title # 文字列を連結して返す
     end
   end
+  
+  def attendance_state(attendance)
+    # 受け取ったAttendanceオブジェクトが当日と一致するか評価します。
+    if Date.current == attendance.worked_on
+      return '出勤' if attendance.started_at.nil?
+      return '退勤' if attendance.started_at.present? && attendance.finished_at.nil?
+    end
+    # どれにも当てはまらなかった場合はfalseを返します。
+    false
+  end
 end
